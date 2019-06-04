d502539d7663a28db5ec7756f9a5864933e5e676
limon <montoyalinaj@gmail.com>
2019-06-04 12:04:16 +0200

    Estructura del proyecto en Eclipse
    Creacion del proyecto usando Maven

---
+++ src/main/java/com/mission/app/App.java
@@ -0,0 +1,13 @@
+package com.mission.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
